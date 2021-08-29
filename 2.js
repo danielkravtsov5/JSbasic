b077a51f5d890682906a2d17a9b4b48b279b06d1
danielkravtsov5 <danielkravtsov5@gmail.com>
2021-08-30 01:07:23 +0300

    2.js

---
+++ 2.js
@@ -0,0 +1,7 @@
+const a = 1; b = -6; c = 9;  // stands for x**2 - 6*x + 9 = 0
+let solutionA = solutionB = 0
+
+solutionA = ((-b)+(Math.sqrt((b*b)-(4*a*c))))/(2*a) ;
+solutionB = ((-b)-(Math.sqrt((b*b)-(4*a*c))))/(2*a) ;
+
+console.log(solutionA, solutionB)
