7ab92f4704a4c2311eea466d5c6c381d6e42c1cf
danielkravtsov5 <danielkravtsov5@gmail.com>
2021-09-01 18:31:37 +0300

    race

--- raceWinners.js
+++ raceWinners.js
@@ -1,37 +1,34 @@
 // list of Formula 1 champions since the year 2000
 const formula1Champions = [
-    "Schumacher",
-    "Schumacher",
-    "Schumacher",
-    "Schumacher",
-    "Schumacher",
-    "Alonso",
-    "Alonso",
-    "Räikkönen",
-    "Hamilton",
-    "Button",
-    "Vettel",
-    "Vettel",
-    "Vettel",
-    "Vettel",
-    "Hamilton",
-    "Hamilton",
-    "Rosberg",
-    "Hamilton",
-    "Hamilton",
-    "Hamilton",
-    "Hamilton"
+  "Schumacher",
+  "Schumacher",
+  "Schumacher",
+  "Schumacher",
+  "Schumacher",
+  "Alonso",
+  "Alonso",
+  "Räikkönen",
+  "Hamilton",
+  "Button",
+  "Vettel",
+  "Vettel",
+  "Vettel",
+  "Vettel",
+  "Hamilton",
+  "Hamilton",
+  "Rosberg",
+  "Hamilton",
+  "Hamilton",
+  "Hamilton",
+  "Hamilton"
 ]
 let winners = {}
 
 function countWin(driver) {
-    if(winners.hasOwnProperty(driver)){
-        winners[driver] 
-      } else {
-       winners[driver] = 1
-     }
+  // your code goes here
+  if (!winners[driver]) winners[driver] = 0 
+  winners[driver]++;
 }
-     
-
 
 formula1Champions.forEach(countWin)
+console.log(winners)
